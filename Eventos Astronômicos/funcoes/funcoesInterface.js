1d5b282077908359ed0c39d50b55234449f0ddbe
Marcelo Rocha Gomes <54919462+MarceloDev1@users.noreply.github.com>
2022-01-10 05:50:55 -0300

    Add files via upload

---
+++ Eventos Astronômicos/funcoes/funcoesInterface.js
@@ -0,0 +1,24 @@
+import { inverteMesAno, retornaHemisferio, retornaIntensidade} from "./funcoesLogicas.js";
+
+const imprimeListaChuva = (listaChuvas) => {
+    console.log('\nNOME DO METEORO             - INTENSIDADE - HEMISFÉRIO - PERÍODO');
+
+    listaChuvas.forEach(imprimeItemListaChuva );
+}
+
+const imprimeItemListaChuva = (chuva) => {
+    const nome = chuva.nome.padEnd(27,' ');
+    let intensidade = retornaIntensidade(chuva.intensidade);
+    let hemisferio = retornaHemisferio(chuva.declinacao);
+
+    intensidade = intensidade.padEnd(11, ' ');
+    hemisferio = hemisferio.padEnd(10, ' ');
+
+    const dataInicio = inverteMesAno(chuva.inicio);
+    const dataFim = inverteMesAno(chuva.fim);
+
+
+    console.log(`${ nome} - ${ intensidade } - ${ hemisferio } - ${ dataInicio } à ${ dataFim } `);
+}
+
+export default imprimeListaChuva;
